9095755f598615f1c94170372afe216bec6acab4
Cauana <77700346+Cauana@users.noreply.github.com>
2022-09-11 12:36:19 -0300

    Add files via upload

--- pythonators/login base/loginpythonators.py
+++ pythonators/login base/loginpythonators.py
@@ -3,14 +3,13 @@ import pandas as pd
 from tkinter import messagebox
 from openpyxl import *
 
-
+#consulta de login na xlsx
 data = pd.read_excel('login.xlsx',engine='openpyxl')
-print(data)
 senha1 = str(data.iloc[0,1])
-print(senha1)
 email1 = str(data.iloc[0,0])
-print(email1)
 
+
+#Função verificação de login inserido e abrir página de avaliação
 def btn_clicked():
     emailcred = str(email.get())
     senhacred = str(senha.get())
@@ -22,61 +21,56 @@ def btn_clicked():
     elif (emailcred != email1 or senhacred != senha1):
         messagebox.showerror(title=None, message='                          Erro!\n Seu usuário ou senha estão incorretos')
 
+#config e design janela root
 window = Tk()
 window.title('Login testes')
-window.geometry("1440x1024")
 window.configure(bg="#F0F0F0")
-canvas = Canvas(
-    window,
-    bg="#F0F0F0",
-    height=1024,
-    width=1440,
-    bd=0,
-    highlightthickness=0,
-    relief="ridge")
-canvas.place(x=0, y=0)
 
-canvas.create_text(
-    795, 560,
-    text="Ao clicar em entrar, ou ao continuar com as outras opções \n abaixo, você concorda com os Termos de serviço e confirma \n que leu a Política de privacidade do Pythonators Database.",
-    fill="#000000",
-    justify=CENTER,
-    font=("None", int(13.0)))
+#definindo largura e altura com base na tela do usuário
 
+largura_screen = window.winfo_screenwidth()
+altura_screen = window.winfo_screenheight()
+largura = largura_screen+20
+altura = altura_screen+1
+posx = largura_screen/2-largura/2
+posy = altura_screen/2-altura/2
+window.geometry('%dx%d+%d+%d' % (largura_screen,altura_screen,posx,posy))
+
+
+#Window design
+create_text = Label(text="Ao clicar em entrar, ou ao continuar com as outras opções \n abaixo, você concorda com os Termos de serviço e confirma \n que leu a Política de privacidade do Pythonators Database.",justify=CENTER, font=("None", int(13.0)))
+create_text.place(relx = 0.5, rely = 0.65, anchor = CENTER)
 
 img0 = PhotoImage(file = f"img0.png")
 img1 = PhotoImage(file="image 1.png")
 label_img1 = Label(window, image=img1)
-label_img1.place(x=660, y=9)
+label_img1.place(relx = 0.5, rely = 0.18, anchor = CENTER)
 
-b0 = Button(
-    image=img0,
-    borderwidth=0,
-    highlightthickness=0,
-    command=btn_clicked,
-    relief="flat")
+#Config botão login
+b0 = Button(image=img0, borderwidth=0, highlightthickness=0, command=btn_clicked, relief="flat")
+b0.place(relx = 0.5, rely = 0.75, anchor = CENTER, width=512, height=64)
 
-b0.place(
-    x=540, y=600,
-    width=512,
-    height=64,)
+#Config entrada de email e senha com placeholder
 
-email = Entry(window,
-              font="Arial 18",
-              bg='#CAD6D6')
-email.place(
-    x=540, y=380,
-    width=518,
-    height=50,)
-senha = Entry(window,
-              font="Arial 18",
-              bg='#CAD6D6',
-              )
-senha.place(x=540, y=450,
-    width=518,
-    height=50)
+def on_click1(event):
+    email.configure(state=NORMAL)
+    email.delete(0, END)
 
+def on_click2(event):
+    senha.configure(state=NORMAL)
+    senha.delete(0,END)
 
+email = Entry(window, font="Arial 18", bg='#F0F0F0')
+email.insert(0,'USUÁRIO')
+email.configure(state=DISABLED)
+email.bind("<Button-1>",on_click1)
+email.place(relx = 0.5, rely = 0.45, anchor = CENTER, width=518, height=50)
 
-window.resizable(True, True)
+senha = Entry(window, font="Arial 18", bg='#F0F0F0',)
+senha.insert(0,'SENHA')
+senha.configure(state=DISABLED)
+senha.bind("<Button-1>",on_click2)
+senha.place(relx=0.5, rely=0.55, anchor=CENTER, width=518, height=50)
+
+window.resizable(False, False)
 window.mainloop()
