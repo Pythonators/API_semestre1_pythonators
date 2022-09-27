fbec315da03cba7f2d775f360bc0e226de62f295
Cauana Dias <77700346+Cauana@users.noreply.github.com>
2022-09-26 23:45:32 -0300

    Atualização de rotas
    Adição das páginas de seleção de sprint e avaliação de alunos

--- API_Sprint2_principal/Website/auth.py
+++ API_Sprint2_principal/Website/auth.py
@@ -10,9 +10,8 @@ def login():
 
 @auth.route("/sprint")
 def pagina_sprint():
-    return "<p>página sprint<\p>"
-    #mensagem="o botão foi acionado"
-    #linkar botão aqui
+    mensagem = "o botão foi acionado"
+    return ("<p>página sprint<\p>",mensagem)
 
 @auth.route('/admin')
 def pagina_admin():
@@ -22,22 +21,22 @@ def pagina_admin():
 def pagina_cadastro_admin():
     return "<p>página cadastro de alunos e grupos<\p>"
 
-@auth.route("/aluno")
-def telaavaliacao():
-    return "<p>página aluno<\p>"
+@auth.route("/aluno/avaliacao")
+def avaliacao():
+    return "<p>página de avaliação aluno<\p>"
+
+@auth.route("/aluno/notas")
+def aluno_notas():
+    return "<p>página de notas aluno<\p>"
 
 @auth.route('/professor-m2')
-def pagina_professorm2():
+def professorm2():
     return "<p>página professor M2<\p>"
 
 @auth.route('/professor-p2')
-def pagina_professorp2():
+def professorp2():
     return "<p>página professor P2<\p>"
 
 @auth.route("/professor/dashboard")
-def pagina_professor_dash():
+def professor_dash():
     return "<p>página dashboard<\p>"
-
-
-
-
