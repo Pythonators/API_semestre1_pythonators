4aae7e2fec0d2654034c8e2759f7b122b43f4434
Mikaéla Begotti <mikaela.cardoso@fatec.sp.gov.br>
2022-10-02 13:45:51 -0300

    quero remover

--- API_PythonatorsFlask2.0/Website/__init__.py
+++ API_PythonatorsFlask2.0/Website/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 def create_app():
     app = Flask(__name__)
@@ -7,14 +6,4 @@ def create_app():
     from Website.rotas import app
 
 
-=======
-from flask import Flask
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'A1B2C3'
-
-    from Website.rotas import app
-
-
->>>>>>> 6165261cd08a060cae6648c8024d20a8d333b3dc
     return app
--- API_PythonatorsFlask2.0/Website/main.py
+++ API_PythonatorsFlask2.0/Website/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 
 from Website import create_app
@@ -7,14 +6,4 @@ app = create_app()
 
 #app = Flask(__name__)
 if __name__ == '__main__':
-=======
-from flask import Flask
-
-from Website import create_app
-
-app = create_app()
-
-#app = Flask(__name__)
-if __name__ == '__main__':
->>>>>>> 6165261cd08a060cae6648c8024d20a8d333b3dc
     app.run(debug=True)
