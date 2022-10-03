3c68bdec860ca5d3db3369d69a12bc8d1e463ba1
Pirajui <81831521+Pirajui@users.noreply.github.com>
2022-10-03 11:59:58 -0300

    Delete Website directory

--- Website/modelos.py
+++
@@ -1,9 +0,0 @@
-class Usuario:
-    def __init__(self, id, cargo, nome, usuario, senha, funcao, time) -> None:
-        self.id=id
-        self.cargo = cargo
-        self.nome=nome
-        self.usuario=usuario
-        self.senha=senha
-        self.funcao=funcao
-        self.time=time
