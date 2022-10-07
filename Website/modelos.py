84d4a00727fce63f81b3db64d97e5f1394b95973
Ryan Lanziloti De Faria Teixeira <81831521+Pirajui@users.noreply.github.com>
2022-10-06 22:16:07 -0300

    Add files via upload

---
+++ Website/modelos.py
@@ -0,0 +1,13 @@
+class Usuario:
+    def __init__(self, id, cargo, nome, usuario, senha, funcao, time) -> None:
+        self.id=id
+        self.cargo = cargo
+        self.nome=nome
+        self.usuario=usuario
+        self.senha=senha
+        self.funcao=funcao
+        self.time=time
+class Avaliacao:
+    def __init__(self, avaliado, perguntas) -> None:
+        self.avaliado=avaliado
+        self.perguntas = perguntas
