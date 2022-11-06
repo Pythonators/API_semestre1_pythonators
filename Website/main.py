5040ee0fd8eb1686c875eee23a549b8e879d731f
Ana Damasceno <107864553+anadamascenos@users.noreply.github.com>
2022-11-06 11:58:32 -0300

    Add files via upload

--- Website/main.py
+++ Website/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from rotas import app
 
 from __init__ import create_app
 
@@ -6,4 +7,4 @@ app = create_app()
 
 #app = Flask(__name__)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  
--- Website/modelos.py
+++ Website/modelos.py
@@ -1,13 +1,35 @@
-class Usuario:
-    def __init__(self, id, cargo, nome, usuario, senha, funcao, time) -> None:
+class Professor:
+    def __init__(self, id, nome, usuario, senha) -> None:
         self.id=id
-        self.cargo = cargo
+        self.nome=nome
+        self.usuario=usuario
+        self.senha=senha
+        #self.funcao=funcao
+class Avaliacao:
+    def __init__(self, avaliado, perguntas) -> None:
+        self.avaliado=avaliado
+        self.perguntas = perguntas
+class Salas:
+    def __init__(self, sala,prof,prof2):
+        self.sala = sala
+        self.prof = prof
+        self.prof2 = prof2
+class Sprint:
+    def __init__(self,sprint,data):
+        self.sprint=sprint
+        self.data=data
+class Times:
+    def __init__(self,id,turma,nome_time):
+        self.id=id
+        self.turma=turma
+        self.nome_time=nome_time
+class Alunos:
+    def __init__(self, id, cargo, nome, usuario, senha, funcao, time,sala) -> None:
+        self.id=id
+        self.cargo=cargo
         self.nome=nome
         self.usuario=usuario
         self.senha=senha
         self.funcao=funcao
         self.time=time
-class Avaliacao:
-    def __init__(self, avaliado, perguntas) -> None:
-        self.avaliado=avaliado
-        self.perguntas = perguntas
+        self.sala=sala
